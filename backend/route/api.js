4cf080e2a89cd52d7fb99039af6c25ce2105b333
Onwordi Prince <princelini4@gmail.com>
2021-04-04 15:06:39 +0100

    redis cache implemented

--- backend/route/api.js
+++ backend/route/api.js
@@ -4,10 +4,12 @@ const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const path = require('path');
 const knex  = require('../db/knex');
+const redis = require('redis');
+const redis_Port = process.env.PORT || 6379;
 
 require("dotenv").config()
 
-//const Product = require('../model/product');
+const client = redis.createClient(redis_Port);
 
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -29,21 +31,26 @@ cloudinary.config({
 
 //localhost:3000/api/products
 router.get('/products', (req, res, next) => {
-    knex.raw('select * from product').then((products) => {
-        res.send(products.rows)
-    }).catch((err) =>console.log(err))
+    client.get('products', (err, items) => {
+        if(err) console.log(err);
+        if(items !== null){
+            JSON.parse(items);
+            res.send(items);
+            console.log('fetched from cache')
+        }else{
+            knex.raw('select * from product').then((products) => {
+                res.send(products.rows);
+                console.log('fetched from postgres')
+                client.setex('products', 100, JSON.stringify(products.rows));
+            }).catch((err) =>console.log(err))
+        }
+    });
 })
 
 router.post('/products', upload, (req, res, next) => {
-    console.log("body " + req.body.name)
-    console.log("file " + req.file)
+    
     let path = req.file.path;
     cloudinary.uploader.upload(path,{folder: "test-two-asset"}).then((result) => {
-        // let newProduct = new Product({
-        //     name: req.body.product,
-        //     price: req.body.price,
-        //     path: result.url
-        // });
         knex.raw('insert into product(product_name, product_price, product_path) values(?, ?, ?)', [req.body.product, req.body.price, result.url])
         .then((doc) => {
             res.send(doc);
