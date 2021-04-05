493c01e9b3b446ed1703cfb0138c527c0cb136fd
Onwordi Prince <princelini4@gmail.com>
2021-04-05 17:01:44 +0100

    .

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -8,8 +8,7 @@ module.exports = {
     connection: {
       database: process.env.db,
       user:     process.env.user,
-      password: process.env.pass,
-      ssl: true
+      password: process.env.pass
     },
     pool: {
       min: 2,
@@ -27,6 +26,9 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
     migrations: {
       directory: __dirname + '/db/migrations'
     },
