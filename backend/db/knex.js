8e2b36b219cc64855eab453e31fe36a99583f681
Onwordi Prince <princelini4@gmail.com>
2021-04-05 18:23:20 +0100

    .

--- backend/db/knex.js
+++ backend/db/knex.js
@@ -1,4 +1,4 @@
 const knex = require('knex');
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV;
 const config = require('../knexfile.js')[environment];
 module.exports = knex(config);
